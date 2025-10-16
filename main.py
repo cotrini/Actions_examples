bdbbf933b49ca6b4e5afbc805222460762a8c0a1
cotrini <89944394+cotrini@users.noreply.github.com>
2025-10-15 19:05:34 -0500

    weather test

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hola........")
+import request
+
+def get_weather():
+    result = request.get("www.ambiente.com/api")
+    return result.json()["temperature"]
+
+    
---
+++ main_test.py
@@ -0,0 +1,16 @@
+from main import get_wather
+import unittest
+from unittest.mock import patch
+
+
+class Test_Weather_class(unittest, TestCase):
+    @patch("main.request.get")
+    def test_get_weather(mock_get):
+        mock_get.return_value.json.return_value = {"temperature": 22}
+
+        result = get_wather()
+        self.assertTrue(result, 22)
+
+if __name__=="__main__":
+    unittest.main()
+       
